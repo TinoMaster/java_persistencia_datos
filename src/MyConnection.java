8e2d7f589aa527464d08e43bc229a9dd715ab392
TinoMaster <oscarmarcos2673@gmail.com>
2024-07-29 23:32:22 +0200

    ok

---
+++ src/MyConnection.java
@@ -0,0 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyConnection {
+
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mensajes_app","root","helloworld");
+
+            if(connection != null) {
+                System.out.println("Connected");
+            }
+        }catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return connection;
+    }
+}
